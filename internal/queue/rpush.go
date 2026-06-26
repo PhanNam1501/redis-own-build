c56205cc787313c46c52caaa480bd811372e70e5
PhanNam1501 <tuannamle256@gmail.com>
2026-06-26 21:58:23 +0700

    codecrafters submit [skip ci]

--- internal/queue/rpush.go
+++ internal/queue/rpush.go
@@ -28,7 +28,7 @@ func (q *queue) Query(key string, l, r int) []string {
 	if r < 0 {
 		return []string{}
 	}
-	if l > size || l > r-1 {
+	if l > size || l > r {
 		return []string{}
 	} else if r > size {
 		r = size
